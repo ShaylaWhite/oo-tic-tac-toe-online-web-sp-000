98489a21fa8b89bcfebd757665349611b749f9ad
Shayla White <shayladwhite1@gmail.com>
2019-06-10 01:59:48 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,14 +1,22 @@
 class TicTacToe
   
-WIN_COMBINATIONS =
-[0,1,2],
-[3,4,5],
-[6,7,8],
-[0,3,6],
-[1,4,7],
-[2,5,8],
-[0,4,8],
-[2,4,6]]
+# CONSTANTS #
+
+  # Provides a nested array of all possible combinations
+  # that would win the game of Tic Tac Toe.
+  WIN_COMBINATIONS = [
+    [0,1,2],  # Top    Row
+    [3,4,5],  # Middle Row
+    [6,7,8],  # Bottom Row
+
+    [0,3,6],  # Left   Column
+    [1,4,7],  # Center Column
+    [2,5,8],  # Right  Column
+
+    [0,4,8],  # Left   Diagonal
+    [2,4,6]   # Right  Diagonal
+  ]
+
   def initialize(board = nil)
     @board = board || Array.new(9, " ")
   end
