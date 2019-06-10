d52b1bd4cb3feeabe7e9add74ed2eb6bb97da528
Shayla White <shayladwhite1@gmail.com>
2019-06-10 02:07:49 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -34,7 +34,10 @@ class TicTacToe
     user_input.to_i - 1
   end
   
-  
+  # Method to place the player's choice onto the @board.
+  def move(index, current_player)
+    @board[index] = current_player
+  end
   
   
 end
