4471a6929da724d0d5fc702bb9f0827da9ddb1b2
Shayla White <shayladwhite1@gmail.com>
2019-06-10 02:17:49 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -55,11 +55,8 @@ def valid_move?(index)
   end
   
   def turn
-   
     puts "Please enter 1-9:"
-   
     input = gets.strip
-   
     index = input_to_index(input)
     
     if valid_move?(index)
